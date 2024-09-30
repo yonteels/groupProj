8707f89e6d9d93ce7713468e2394f6e6c59863f3
Yonteel <leetony0612@gmail.com>
2024-09-30 18:43:15 -0400

    fix routing error 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import {Component} from '@angular/core';
 import {RouterOutlet, RouterLink} from '@angular/router';
-
+import {NavBarComponent} from './nav-bar/nav-bar.component';
 @Component({
   selector: 'app-root',
   template: `
 
-    <nav-bar></nav-bar>
-    
+    <app-nav-bar></app-nav-bar>
     <router-outlet />
   `,
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NavBarComponent],
 })
 export class AppComponent {
   title = 'groupProj'; 
